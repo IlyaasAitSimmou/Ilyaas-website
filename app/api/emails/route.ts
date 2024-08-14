9a5dd05f4cb954c8079e8c15971b3fafd4d0ec8a
IlyaasIsmail Simmou <ilyaasismailsimmou@macbook-air-2.home>
2024-08-13 23:41:42 -0400

    testing for error #3

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -1,6 +1,7 @@
 import { sendEmail } from "@/utils/mail.utils";
 
 export async function POST(request: Request) {
+    console.log(process.env.EMAIL_HOST, process.env.EMAIL_USER, process.env.EMAIL_PASS)
     let reqJson = await request.json();
     console.log(reqJson)
     const user = reqJson.username
